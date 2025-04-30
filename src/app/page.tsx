7c4918ecbe4f84e4e14a29d848121d58bfbf8a6c
Viktor Čordaš <viktor.cordas@gmail.com>
2025-04-30 16:01:41 +0200

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,16 @@ async function Page() {
 
   return (
     <>
-      <div className="sticky top-0 flex flex-col p-6 items-end">
-        <div className="flex justify-end gap-6">
+      <div className='sticky top-0 flex flex-col p-6 items-end'>
+        <div className='flex justify-end gap-6'>
           {hasPermission(user, 'galleries', 'read') && (
-            <Link href="/dashboard">Dashboard</Link>
+            <Link href='/dashboard'>Dashboard</Link>
           )}
           <Authentication />
         </div>
       </div>
-      <div className="fixed bottom-0 w-full h-[60vh] flex flex-col justify-between">
-        <h1 className="text-6xl tracking-tighter text-center">Glimpsee</h1>
+      <div className='fixed bottom-0 w-full h-[60vh] flex flex-col justify-between'>
+        <h1 className='text-6xl tracking-tighter text-center'>GG-28</h1>
         <Actions />
       </div>
     </>
